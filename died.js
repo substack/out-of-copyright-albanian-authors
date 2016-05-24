c281cd7e4baf5eeefae87f82cf23f97c1cf9857f
James Halliday <mail@substack.net>
2016-05-24 14:30:49 +0200

    improve death calculation with year range

--- died.js
+++ died.js
@@ -1,6 +1,7 @@
 var vdiqme = /vdiq\s+më\s+(?:\[*\d+\s+\w+\]*\s+)?\[*(\d{4})\]*/i
 var vdekja = /(?:died|DV|ditëvdekja)\s*=\s*(?:\[*\d+\s+\w+\]*\s+)?\[*(\d{4})\]*/i
 var xvdekja = /(?:died|DV|ditëvdekja)\s*=\s*{{(.+?)}}/i
+var yrange = /\b\[*(\d{3,4})\]*\s+[-–]\s+\[*(\d{3,4})\]*\b/
 
 module.exports = function (body) {
   var m
@@ -9,7 +10,15 @@ module.exports = function (body) {
   m = vdekja.exec(body)
   if (m) return m[1]
   m = xvdekja.exec(body)
-  if (m) return Math.max.apply(null, m[1].split('|').map(Number).filter(ok))
+  if (m) {
+    var parts = m[1].split('|').map(Number).filter(ok)
+    return Math.max.apply(null, parts)
+  }
+  m = yrange.exec(body)
+  if (m) {
+    var b = Number(m[1]), d = Number(m[2])
+    if (d > b && d - b > 20) return d
+  }
   return null
 }
 
